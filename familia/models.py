f9ce55365a40d71c663cfdb27af7e9c4d9c37bdc
Juan Flores <sacheflores@gmail.com>
2023-11-03 11:12:24 -0300

    ya pude migrar

--- familia/models.py
+++ familia/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=256)
+    apellido = models.CharField(max_length=256)
+    parentezco_conmigo = models.CharField(max_length=128)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    dni = models.IntegerField(null=True, blank=True)
